ddc988ba6db7ceef3366084acdd4954bddaf0d3c
mimitch2 <mike@melodix.com>
2018-05-24 12:18:25 -0500

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,63 @@
 import React from "react";
+import "../App.css";
 
-function Header() {
-  return (<div />);
-}
+const Header = () => {
+  return (
+    <div className="header">
+	
+      <div className="top-header">
+        <div className="top-header-left">
+          <ul>
+            <li className="active"><a href="#">afsad</a></li>
+            <li><a href="#">Specials</a></li>
+            <li><a href="#">Delivery</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+        <div className="top-header-right">
+          <ul>
+            <li><a href="#">CURRENCY:</a></li>
+            <li>
+              <select>
+                <option>Dollar</option>
+                <option>Euro</option>
+                <option>Pound</option>
+              </select>
+            </li>
+          </ul>
+        </div>
+        <div className="clear"> </div>
+      </div>
+      <div className="clear"> </div>
+      <div className="sub-header">
+        <div className="logo">
+          <a href="index.html"><img src="images/logo.png" title="logo" alt="" /></a>
+        </div>
+        <div className="sub-header-right">
+          <ul>
+            <li><a href="#">log in</a></li>
+            <li><a href="#">Your account</a></li>
+            <li><a href="#">CART: (EMPTY) <img src="images/cart.png" title="cart" alt="" /></a></li>
+          </ul>
+          <input type="text" /><input type="submit" value="search" />
+        </div>
+        <div className="clear"> </div>
+      </div>
+      <div className="clear"> </div>
+      <div className="top-nav">
+        <ul>
+          <li><a href="carlights.html">car lights</a></li>
+          <li><a href="carwheels.html">Car wheels</a></li>
+          <li><a href="carbumpers.html">car bumpers</a></li>
+          <li><a href="caradsystem.html">car audiosystem</a></li>
+          <li><a href="truckbumpers.html">Truck bumpers</a></li>
+          <li><a href="contact.html">Feedback</a></li>
+          <div className="clear"> </div>
+        </ul>
+      </div>
+		
+    </div>
+  );
+};
 
 export default Header;
---
+++ src/components/SubHeader.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import "../App.css";
+
+
+const SubHeader = () => {
+  return (
+    
+    <div className="sub-header">
+      <div className="clear"> </div>
+      <div className="logo">
+        <a href="index.html"><img src="images/logo.png" title="logo" alt="" /></a>
+      </div>
+      <div className="sub-header-right">
+        <ul>
+          <li><a href="#">log in</a></li>
+          <li><a href="#">Your account</a></li>
+          <li><a href="#">CART: (EMPTY) <img src="images/cart.png" title="cart" alt="" /></a></li>
+        </ul>
+        <input type="text" /><input type="submit" value="search" />
+      </div>
+      <div className="clear"> </div>
+    </div>
+
+  );
+};
+ 
+export default SubHeader;
+
+
+
+
+
+
+
+
+
+
