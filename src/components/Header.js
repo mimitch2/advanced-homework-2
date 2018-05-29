45f59e9d624623da26b0bfb4693c5da4271a2b24
mimitch2 <mike@melodix.com>
2018-05-29 07:40:15 -0500

    more components

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,20 +29,7 @@ const Header = () => {
         <div className="clear"> </div>
       </div>
       <div className="clear"> </div>
-      <div className="sub-header">
-        <div className="logo">
-          <a href="index.html"><img src="images/logo.png" title="logo" alt="" /></a>
-        </div>
-        <div className="sub-header-right">
-          <ul>
-            <li><a href="#">log in</a></li>
-            <li><a href="#">Your account</a></li>
-            <li><a href="#">CART: (EMPTY) <img src="images/cart.png" title="cart" alt="" /></a></li>
-          </ul>
-          <input type="text" /><input type="submit" value="search" />
-        </div>
-        <div className="clear"> </div>
-      </div>
+      
       <div className="clear"> </div>
       <div className="top-nav">
         <ul>
